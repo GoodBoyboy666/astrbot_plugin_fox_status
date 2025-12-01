22d00ce56157c0dd1889e1b27f53da10610eed78
GoodBoyboy <me@goodboyboy.top>
2025-12-01 18:01:35 +0800

    调整线程阻塞问题

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import asyncio
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
 from .collector import get_all_info
 
-@register("astrbot_plugin_fox_status", "GoodBoyboy", "一个用于获取 AstrBot 机器状态的插件。", "1.0.0")
+@register("astrbot_plugin_fox_status", "GoodBoyboy", "一个用于获取 AstrBot 机器状态的插件。", "1.0.1")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -16,7 +17,8 @@ class MyPlugin(Star):
     async def status(self, event: AstrMessageEvent):
         """获取状态"""
         event.stop_event()
-        yield event.plain_result(get_all_info())
+        result = await asyncio.to_thread(get_all_info)
+        yield event.plain_result(result)
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
